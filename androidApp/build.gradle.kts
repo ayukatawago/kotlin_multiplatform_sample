60e059641ee812daf758b703246a7800a8152fb3
ayukatawago <46732412+ayukatawago@users.noreply.github.com>
2021-07-06 23:11:29 +0900

    [Android] Fix build error

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -28,4 +28,8 @@ android {
             isMinifyEnabled = false
         }
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
