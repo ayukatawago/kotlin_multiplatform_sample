ffafcd904eaf551ad4c90b9f91b29165df9a1d93
ayukatawago <46732412+ayukatawago@users.noreply.github.com>
2021-07-06 23:10:25 +0900

    [JS] Use React DOM

--- jsApp/build.gradle.kts
+++ jsApp/build.gradle.kts
@@ -6,12 +6,19 @@ group = "com.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-js-wrappers")
     mavenCentral()
 }
 
 dependencies {
     implementation(project(":shared"))
     implementation(kotlin("stdlib-js"))
+
+    //React, React DOM + Wrappers
+    implementation("org.jetbrains:kotlin-react:17.0.2-pre.154-kotlin-1.5.0")
+    implementation("org.jetbrains:kotlin-react-dom:17.0.2-pre.154-kotlin-1.5.0")
+    implementation(npm("react", "17.0.2"))
+    implementation(npm("react-dom", "17.0.2"))
 }
 
 kotlin {
--- jsApp/src/main/kotlin/main.kt
+++ jsApp/src/main/kotlin/main.kt
@@ -1,7 +1,41 @@
 import com.example.shared.Platform
 import kotlinx.browser.document
+import react.dom.*
 
 fun main() {
     val platform = Platform().platform
-    document.write("Hello, $platform")
+
+    val videoList = listOf(
+        KotlinVideo(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
+        KotlinVideo(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
+    )
+    render(document.getElementById("root")) {
+        h1 {
+            +"Hello, $platform"
+        }
+        div {
+            h3 {
+                +"Video List"
+            }
+            videoList.forEach { video ->
+                p {
+                    +"${video.speaker}: ${video.title}"
+                }
+            }
+        }
+    }
 }
+
+external interface Video {
+    val id: Int
+    val title: String
+    val speaker: String
+    val videoUrl: String
+}
+
+data class KotlinVideo(
+    override val id: Int,
+    override val title: String,
+    override val speaker: String,
+    override val videoUrl: String
+): Video
