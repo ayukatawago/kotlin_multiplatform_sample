953ef01b998f5d7f141b2e1474233299eeadb1e7
ayukatawago <46732412+ayukatawago@users.noreply.github.com>
2021-07-06 23:10:07 +0900

    [Android] Update SDK and dependency version

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -9,17 +9,17 @@ version = "1.0-SNAPSHOT"
 
 dependencies {
     implementation(project(":shared"))
-    implementation("com.google.android.material:material:1.2.1")
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.2")
+    implementation("com.google.android.material:material:1.3.0")
+    implementation("androidx.appcompat:appcompat:1.3.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
 }
 
 android {
-    compileSdkVersion(29)
+    compileSdkVersion(30)
     defaultConfig {
         applicationId = "com.example.androidApp"
         minSdkVersion(24)
-        targetSdkVersion(29)
+        targetSdkVersion(30)
         versionCode = 1
         versionName = "1.0"
     }
