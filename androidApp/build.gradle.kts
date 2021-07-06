d00b845ccd7897df09b0cf63bcd8262f472139ba
ayukatawago <46732412+ayukatawago@users.noreply.github.com>
2021-07-06 23:11:46 +0900

    [Android] Update library

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     val fragmentVersion = "1.3.4"
     val navigationVersion = "2.3.5"
     implementation(project(":shared"))
-    implementation("com.google.android.material:material:1.3.0")
+    implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.3.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
     implementation("androidx.fragment:fragment:$fragmentVersion")
